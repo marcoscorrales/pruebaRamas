f52e26ee50de6f11cd202226f962596cc6adbff2
marcos <marcoscorralescastro.daw2020@riberadeltajo.es>
2022-02-10 12:58:46 +0100

    cambio_en_la_rama

--- src/components/Articulo1/Articulo1.jsx
+++ src/components/Articulo1/Articulo1.jsx
@@ -6,7 +6,7 @@ export const Articulo1 = () => {
   return (
     <article className="articulo1">
         <h2 className="articulo__h2">Supervisor</h2>
-        <p className="articulo__p">Lorem ipsum dolor sit amet consectetur adipisicing elit.Holaa</p>
+        <p className="articulo__p">Lorem ipsum dolor sit amet consectetur adipisicing elit.Primera rama</p>
         <img className="articulo__img" src={img} alt="imagen" />
     </article>
   )
